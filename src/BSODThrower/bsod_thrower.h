636eae656447dbbb0e3e500f805d5762b02bce20
myanwc <anton.kostyrev@yandex.ru>
2026-02-16 17:43:59 +0300

    Add system errors codes

--- src/BSODThrower/bsod_thrower.h
+++ src/BSODThrower/bsod_thrower.h
@@ -6,6 +6,11 @@ namespace maltech {
 
     namespace bsod {
 
+#define STATUS_FLOAT_MULTIPLE_FAULTS     0xC0000094
+#define STATUS_ASSERTION_FAILURE         0xC0000420
+#define STATUS_SYSTEM_PROCESS_TERMINATED 0xC000021A  
+#define STATUS_DATA_CORRUPTION_ERROR     0xC00002C4 
+
         class BSODThrower {
         public:
             void TriggerHardError();
