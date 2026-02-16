c4810643aa8d888db6451764c17a1180ccc08243
myanwc <anton.kostyrev@yandex.ru>
2026-02-16 19:04:21 +0300

    Add logging

--- src/BSODThrower/bsod_thrower.cpp
+++ src/BSODThrower/bsod_thrower.cpp
@@ -1,5 +1,6 @@
 #include "bsod_thrower.h"
 #include "../MalwareTechniques/ntdll.h"
+#include "../Logger/logger.h"
 
 #include <Windows.h>
 
@@ -8,29 +9,30 @@
 namespace maltech {
 
     namespace bsod {
+
         BSODThrower::BSODThrower(ntdll::NtDll& ntdll)
             : ntdll_(ntdll)
         {
         }
 
         void BSODThrower::TriggerHardError() {
-            std::cout << "WARNING!!!\n"
-                << "This feature will send a FATAL system error!!!\n"
-                << "This will HARD REBOOT your operating system!!!\n"
-                << "All unsaved data will be lost!!!\n"
-                << "PLEASE use it ONLY on Virtual Machine for testing you EDR or AV system\n"
-                << "Type OK for coninue\n"
-                << "Type whatever else for distard\n"
-                << "Input: ";
+            LOG_CRITICAL("WARNING!!!");
+            LOG_CRITICAL("This feature will send a FATAL system error!!!");
+            LOG_CRITICAL("This will HARD REBOOT your operating system!!!");
+            LOG_CRITICAL("All unsaved data will be lost!!!");
+            LOG_CRITICAL("PLEASE use it ONLY on Virtual Machine for testing you EDR or AV system");
+            LOG_INFO("Type OK for coninue");
+            LOG_INFO("Type whatever else for distard");
+            LOG_INFO("Waiting for input: ");
 
             std::string user_input;
             std::cin >> user_input;
+            LOG_DEBUG("User input: "s.append(user_input));
             if (user_input != "OK") {
                 std::cout << "\nCanceled" << std::endl;
                 return;
             }
 
-            BOOLEAN was_enabled;
             ULONG response;
 
             ntdll_.NtRaiseHardError(
