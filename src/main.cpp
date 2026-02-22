75d9f71795835abba9b26aabd6d6c726f9b21823
myanwc <anton.kostyrev@yandex.ru>
2026-02-22 20:05:42 +0300

    Privilege escalating returning bool value for success check in GUI that used malvac dll

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,14 @@ int main() {
     logging::Logger logger;
     logger.Init();
     try {
-        malvac::application::Application app;
-        app.EscalatePrivilegesToDebug();
-        
-        while (true) {
+        bool res = malvac::application::EscalatePrivilegesToTCB();
+        std::string ress = res ? "success" : "Error";
+        LOG_INFO(ress);
+
+        int pause_sec = 5;
+        while (pause_sec--) {
             Sleep(1000);
         }
-        //app.EmulateRansomware();
-        //app.ThrowBSOD();
-
     }
     catch (const std::exception& e) {
         std::cout << "EDR stress test error: " << e.what() << std::endl;
