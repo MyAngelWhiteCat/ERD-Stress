b789a5c2ce29812a49e5effe2b6d3402b5df0ac3
myanwc <anton.kostyrev@yandex.ru>
2026-02-23 20:35:06 +0300

    Update thread pool to last version

--- src/ThreadPool/thread_pool.h
+++ src/ThreadPool/thread_pool.h
@@ -8,6 +8,10 @@
 #include <stdexcept>
 #include <utility>
 #include <vector>
+#include <string_view>
+#include <syncstream>
+#include <iostream>
+
 #include <Windows.h>
 
 
@@ -28,12 +32,21 @@ private:
     std::vector<HANDLE> hThreads_;
     std::atomic_bool destruction_flag_{ false };
 
+    std::function<void(const std::string_view)> debug_logger_;
+    std::function<void(const std::string_view)> error_logger_;
+    std::mutex default_logger_mutex_;
+
     static DWORD WINAPI ThreadStartWrapper(LPVOID lpParam) {
         static_cast<ThreadPool*>(lpParam)->Work();
         return 0;
     }
 
     void Work();
+
+    template<typename Logger>
+    void SetDebugLogger(Logger&& logger);
+    template<typename Logger>
+    void SetErrorLogger(Logger&& logger);
 };
 
 template<typename Fn>
@@ -46,4 +59,14 @@ inline void ThreadPool::AddTask(Fn&& task) {
         tasks_.push(std::forward<Fn>(task));
     }
     convar_.notify_one();
+}
+
+template<typename Logger>
+inline void ThreadPool::SetDebugLogger(Logger&& logger) {
+    debug_logger_ = std::forward<Logger>(logger);
+}
+
+template<typename Logger>
+inline void ThreadPool::SetErrorLogger(Logger&& logger) {
+    error_logger_ = std::forward<Logger>(logger);
 }
